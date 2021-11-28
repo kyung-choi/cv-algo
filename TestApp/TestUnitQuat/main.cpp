85ab21e0f55b08ef4fa539c258def8da08400e72
KyungYung Choi <KyungYung.Choi@zimmercas.com>
2021-11-27 23:45:37 -0500

    [PoseEstLu] Refactored. Test PoseEstLu.

--- TestApp/TestUnitQuat/main.cpp
+++ TestApp/TestUnitQuat/main.cpp
@@ -8,30 +8,110 @@
 
 using namespace pcl::registration;
 
+typedef pcl::PointUV UV;
+typedef pcl::PointCloud<UV> PointCloudUV;
 typedef pcl::PointXYZ XYZ;
 typedef pcl::PointCloud<XYZ> PointCloudXYZ;
 
-static std::random_device rd;
+void addNoise(PointCloudUV::Ptr _pts, const float _level);
+void addNoise(PointCloudXYZ::Ptr _pts, const float _level);
 
-void addGaussianNoise(PointCloudXYZ::Ptr _pts, const double _stdev);
+void generatePoseEstDataSet(const float _fx, const float _fy, const float _ppx, const float _ppy,
+	const PointCloudXYZ& _xyz, PointCloudUV& _uv, Eigen::Matrix4f& _Rt);
 
-Eigen::Matrix4d getTransformation(const double _w, const double _x, const double _y, const double _z,
-	const double _tx, const double _ty, const double _tz, const double _scale);
+Eigen::Matrix4f getTransformation(const float _w, const float _x, const float _y, const float _z,
+	const float _tx, const float _ty, const float _tz, const float _scale = 1.f);
+
+void testAbsOrientUQ();
+void testPoseEstLu();
 
 int main ()
 {
-	ky::PoseEstLu poseEst;
+	try
+	{
+		testAbsOrientUQ();
+		testPoseEstLu();
+	}
+	catch (const std::runtime_error& rt_err)
+	{
+		std::cout << rt_err.what() << std::endl;
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
 
+	return 0;
+}
+
+void addNoise(PointCloudUV::Ptr _pts, const float _level)
+{
+	Eigen::MatrixXf random = Eigen::MatrixXf::Random(2, _pts->size());
+	random *= _level;
+	int i{ 0 };
+	for (auto iter{ _pts->points.begin() }; iter != _pts->points.end(); ++iter)
+	{
+		iter->u += random(0, i);
+		iter->v += random(1, i++);
+	}
+}
+
+void addNoise(PointCloudXYZ::Ptr _pts, const float _level)
+{
+	Eigen::MatrixXf random = Eigen::MatrixXf::Random(3, _pts->size());
+	random *= _level;
+	int i{ 0 };
+	for (auto iter{ _pts->points.begin() }; iter != _pts->points.end(); ++iter)
+	{
+		iter->x += random(0, i);
+		iter->y += random(1, i);
+		iter->z += random(2, i++);
+	}
+}
+
+void generatePoseEstDataSet(const float _fx, const float _fy, const float _ppx, const float _ppy,
+	const PointCloudXYZ& _xyz, PointCloudUV& _uv, Eigen::Matrix4f& _Rt)
+{
+	// nominal camera pose transformation.
+	_Rt = Eigen::Matrix4f::Identity();
+	_Rt(1, 1) = _Rt(2, 2) = -1;
+	_Rt.block<3, 1>(0, 3) = Eigen::Vector3f(1, -10, 1050);
+
+	PointCloudXYZ::Ptr xyzc(new PointCloudXYZ);
+	pcl::transformPointCloud(_xyz, *xyzc, _Rt);
+
+	_uv.clear();
+	UV pt;
+	for (const auto& wc : xyzc->points)
+	{
+		pt.u = (_fx * wc.x + _ppx * wc.z) / wc.z;
+		pt.v = (_fy * wc.y + _ppy * wc.z) / wc.z;
+		_uv.push_back(pt);
+	}
+}
+
+Eigen::Matrix4f getTransformation(const float _w, const float _x, const float _y, const float _z,
+	const float _tx, const float _ty, const float _tz, const float _scale)
+{
+	Eigen::Matrix4f transform = Eigen::Matrix4f::Identity();
+	transform.block<3, 3>(0, 0) = _scale * Eigen::Quaternionf(_w, _x, _y, _z).toRotationMatrix();
+	transform.block<3, 1>(0, 3) = Eigen::Vector3f(_tx, _ty, _tz);
+
+	return transform;
+}
+
+void testAbsOrientUQ()
+{
 	// Setup a ground truth transformation
-	double qw{ 0.9988145 }, qx{ 0.0256408 }, qy{ -0.0395854 }, qz{ 0.0120474 };  // keep transformation envelop small 
-	double tx{ -4.9 }, ty{ 3.1 }, tz{ 2.4 }, scale{ 0.99 };                      // for ICP test.
-	Eigen::Matrix4d transform = getTransformation(qw, qx, qy, qz, tx, ty, tz, scale);
+	float qw{ 0.9988145f }, qx{ 0.0256408f }, qy{ -0.0395854f }, qz{ 0.0120474f };  // keep transformation envelop small 
+	float tx{ -4.9f }, ty{ 3.1f }, tz{ 2.4f }, scale{ 0.99f };                      // for ICP test.
+	Eigen::Matrix4f transform = getTransformation(qw, qx, qy, qz, tx, ty, tz, scale);
 
 	// Load source point cloud
 	boost::filesystem::path path = boost::filesystem::current_path();
 	path += boost::filesystem::path("/../../../data/UQ/xyz.pcd");
 	if (!boost::filesystem::exists(path))
-		return 1;
+		throw std::runtime_error("File not found, " + path.string() + ", in testAbsOrientUQ().");
 
 	PointCloudXYZ::Ptr cloud_src(new PointCloudXYZ);
 	pcl::io::loadPCDFile(path.string(), *cloud_src);
@@ -41,14 +121,17 @@ int main ()
 	pcl::transformPointCloud(*cloud_src, *cloud_tgt, transform);
 
 	// Apply additive noise to the target point cloud.
-	double noise_stdev{ 1E-1 };
-	addGaussianNoise(cloud_tgt, noise_stdev);
+	float noise_level{ 0.3f };
+	addNoise(cloud_tgt, noise_level);
 
-	Eigen::Matrix4d transform_uq = Eigen::Matrix4d::Identity();
+	Eigen::Matrix4f transform_uq = Eigen::Matrix4f::Identity();
 
 	// Solve absolute orientation using unit quaternion, Horn's method (simplified version).
-	TransformationEstimation<XYZ, XYZ, double>::Ptr teuq(new TransformationEstimationUQ<XYZ, XYZ, double>);
+	TransformationEstimation<XYZ, XYZ, float>::Ptr teuq(new TransformationEstimationUQ<XYZ, XYZ, float>);
 	teuq->estimateRigidTransformation(*cloud_src, *cloud_tgt, transform_uq);
+
+	std::cout << "g.t. transformation: \n" << transform << std::endl;
+	std::cout << "estimated transformation: \n" << transform_uq << std::endl;
 
 	// Evaluation.
 	float distance{ 0 };
@@ -61,39 +144,53 @@ int main ()
 	distance /= cloud_src->size();
 
 	// TransformationEstimationUQ being part of ICP pipeline.
-	pcl::IterativeClosestPoint<XYZ, XYZ, double> icp;
+	pcl::IterativeClosestPoint<XYZ, XYZ, float> icp;
 	icp.setTransformationEstimation(teuq);
 	icp.setInputSource(cloud_src);
 	icp.setInputTarget(cloud_tgt);
 	icp.align(*cloud_aligned);
-	Eigen::Matrix4d transform_icp = icp.getFinalTransformation();
+	Eigen::Matrix4f transform_icp = icp.getFinalTransformation();
 	auto fitness = icp.getFitnessScore();
 
-	return 0;
+	std::cout << "estimated transformation in icp pipeline: \n" << transform_icp << std::endl;
 }
 
-void addGaussianNoise(PointCloudXYZ::Ptr _pts, const double _stdev)
+void testPoseEstLu()
 {
-	std::mt19937 gen(rd());
-	std::normal_distribution<> distrib(0, _stdev);
-	for (auto iter{ _pts->points.begin() }; iter != _pts->points.end(); ++iter)
-	{
-		iter->x += distrib(gen);
-		iter->y += distrib(gen);
-		iter->z += distrib(gen);
-	}
-}
+	// Pose estimation test
+	boost::filesystem::path path = boost::filesystem::current_path();
+	path += boost::filesystem::path("/../../../data/UQ/xyz.pcd");
+	if (!boost::filesystem::exists(path))
+		throw std::runtime_error("File not found, " + path.string() + ", in testPoseEstLu().");
 
-Eigen::Matrix4d getTransformation(const double _w, const double _x, const double _y, const double _z,
-	const double _tx, const double _ty, const double _tz, const double _scale)
-{
-	Eigen::Quaternion<double> q(_w, _x, _y, _z);
-	Eigen::Matrix3d R = q.toRotationMatrix();
-	Eigen::Vector3d t;
-	t << _tx, _ty, _tz;
-	Eigen::Matrix4d transform = Eigen::Matrix4d::Identity();
-	transform.block<3, 3>(0, 0) = _scale * R;
-	transform.block<3, 1>(0, 3) = t;
+	PointCloudXYZ::Ptr cloud_xyz(new PointCloudXYZ);
+	pcl::io::loadPCDFile(path.string(), *cloud_xyz);
 
-	return transform;
+	// Intrinsic camera parameters
+	const float fx{ 4500 }, fy{ 4500 }, ppx{ 512 }, ppy{ 512 };
+
+	PointCloudUV::Ptr cloud_uv(new PointCloudUV);  // ground-truth image coordinates.
+	Eigen::Matrix4f Rt;  // ground-truth camera pose.
+	generatePoseEstDataSet(fx, fy, ppx, ppy, *cloud_xyz, *cloud_uv, Rt);
+
+	// Initial guess of camera pose is off from ground-truth; by applying an error transformation below.
+	float qw{ 0.9933643f }, qx{ 0.0376655f }, qy{ -0.0688858f }, qz{ 0.0840444f };
+	float tx{ 25.f }, ty{ -35.f }, tz{ 30.f };
+
+	// Initial geuss of camera pose is, in general, from a linear method and inaccurate.
+	Eigen::Matrix4f Rt_est = getTransformation(qw, qx, qy, qz, tx, ty, tz) * Rt;
+
+	// Adding noise to image coordinate.
+	addNoise(cloud_uv, 0.3f);
+
+	std::cout << "ground-truth:\n" << Rt << std::endl;
+	std::cout << "initial guess:\n" << Rt_est << std::endl;
+
+	// Estimate camera pose with noisy measurements and an initial guess.
+	ky::PoseEstLu poseEst;
+	poseEst.setFocalLength(fx, fy);
+	poseEst.setPP(ppx, ppy);
+	poseEst.estimatePose(*cloud_xyz, *cloud_uv, Rt_est);
+
+	std::cout << "estimated:\n" << Rt_est << std::endl;
 }
--- src/pose_est_Lu.cpp
+++ src/pose_est_Lu.cpp
@@ -5,32 +5,29 @@ namespace ky
 	PoseEstLu::PoseEstLu() : m_fx(4500), m_fy(4500), m_ppx(512), m_ppy(512), 
 		m_maxIter(50), m_convergence(0.01)	{}
 
-	void PoseEstLu::estimatePose(const PointCloudXYZ& _model, 
-		const PointCloudUV& _uv, Matrix4f& _transformation) const
+	float PoseEstLu::estimatePose(const PointCloudXYZ& _model, 
+		const PointCloudUV& _uv, Matrix4f& _transformation)
 	{
 		auto iter{ 0 };
-		auto convergence{ std::numeric_limits<float>::max() };
-		auto err{ std::numeric_limits<float>::max() };
-
+		auto convergence{ std::numeric_limits<float>::max() }, err{ std::numeric_limits<float>::max() };
 		pcl::registration::TransformationEstimationSVD<XYZ, XYZ> te;
-		std::vector<Matrix3f, aligned_allocator<Matrix3f>> V(_uv.size(), Matrix3f::Identity());
-
-		for (auto i{ 0 }; i < _uv.size(); ++i)
-			_computeV(_uv[i], V[i]);
-
-		Matrix3f L{ _computeL(V) };
 		PointCloudXYZ Vq;
+
+  	_computeV(_uv);
+		_computeL();
 
 		while (iter++ < m_maxIter && convergence > m_convergence)
 		{
-			_computeT(_model, L, V, _transformation);
-			_computeVq(_model, V, Vq, _transformation);
+			_computeT(_model, _transformation);
+			_computeVq(_model, _transformation, Vq);
 			te.estimateRigidTransformation(_model, Vq, _transformation);
 
 			auto errNew{ _computeErr(_model, Vq, _transformation) };
 			convergence = std::abs(err - errNew);
 			err = errNew;
 		}
+
+		return err;
 	}
 
 	float PoseEstLu::_computeErr(const PointCloudXYZ& _p, const PointCloudXYZ& _Vq, 
@@ -39,40 +36,42 @@ namespace ky
 		float err{ 0 };
 		PointCloudXYZ q;
 		pcl::transformPointCloud(_p, q, _transformation);
+		auto qIter{ q.begin() }; 
+		auto VqIter{ _Vq.begin() };
 
-		for (auto i{ 0 }; i < _p.size(); ++i)
-			err += pcl::squaredEuclideanDistance(q[i], _Vq[i]);
+		for (; qIter!= q.end(); ++qIter, ++VqIter)
+			err += pcl::squaredEuclideanDistance(*qIter, *VqIter);
 
 		return err;
 	}
 
-	void PoseEstLu::_computeV(const UV& _uv, Matrix3f& _V) const
+	void PoseEstLu::_computeV(const PointCloudUV& _uv)
 	{
-		Vector3f v((_uv.u - m_ppx) / m_fx, (_uv.v - m_ppy) / m_fy, 1);
-		_V = v * v.transpose() / (v(0) * v(0) + v(1) * v(1) + 1);
+		for (const auto& uv : _uv.points)
+		{
+			Vector3f v((uv.u - m_ppx) / m_fx, (uv.v - m_ppy) / m_fy, 1);
+			m_V.push_back(v * v.transpose() / (v(0) * v(0) + v(1) * v(1) + 1));
+		}
 	}
 
-	void PoseEstLu::_computeT(const PointCloudXYZ& _p, const Matrix3f& _L,
-		const std::vector<Matrix3f, aligned_allocator<Matrix3f>> _V,
-		Matrix4f& _transformation) const
+	void PoseEstLu::_computeT(const PointCloudXYZ& _p, Matrix4f& _transformation) const
 	{
 		Matrix3f I{ Matrix3f::Identity() }, R{ _transformation.block<3, 3>(0, 0) };
 		Vector3f rhs{ Vector3f::Zero() };
 
 		for (size_t i{ 0 }; i < _p.size(); ++i)
-			rhs += (_V[i] - I) * R * Vector3f(_p[i].x, _p[i].y, _p[i].z);
+			rhs += (m_V[i] - I) * R * Vector3f(_p[i].x, _p[i].y, _p[i].z);
 
-		_transformation.block<3, 1>(0, 3) = _L * rhs;
+		_transformation.block<3, 1>(0, 3) = m_L * rhs;
 	}
 
-	void PoseEstLu::_computeVq(const PointCloudXYZ& _p,
-		const std::vector<Matrix3f, aligned_allocator<Matrix3f>> _V,
-		PointCloudXYZ& _Vq, const Matrix4f& _transformation) const
+	void PoseEstLu::_computeVq(const PointCloudXYZ& _p, 
+		const Matrix4f& _transformation, PointCloudXYZ& _Vq) const
 	{
 		PointCloudXYZ q;
 		pcl::transformPointCloud(_p, q, _transformation);
 		auto qIter{ q.begin() };
-		auto VIter{ _V.begin() };
+		auto VIter{ m_V.begin() };
 		_Vq.clear();
 		
 		for (; qIter != q.end(); ++qIter, ++VIter)
@@ -82,15 +81,14 @@ namespace ky
 		}
 	}
 
-	Eigen::Matrix3f PoseEstLu::_computeL(const std::vector<Matrix3f, 
-		aligned_allocator<Matrix3f>> _V) const
+	void PoseEstLu::_computeL()
 	{
 		Matrix3f S = Matrix3f::Zero();
 
-		for (auto i{ 0 }; i < _V.size(); ++i)
-			S += _V[i];
+		for (auto i{ 0 }; i < m_V.size(); ++i)
+			S += m_V[i];
 
-		auto n{ static_cast<float>(_V.size()) };
-		return (Matrix3f::Identity() - S / n).inverse() / n;
+		auto n{ static_cast<float>(m_V.size()) };
+		m_L = (Matrix3f::Identity() - S / n).inverse() / n;
 	}
 }
