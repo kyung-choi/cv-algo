fc447fb5904efdbe403b7c1d1cf352ca17c622da
KyungYung Choi <KyungYung.Choi@zimmercas.com>
2022-01-06 00:54:54 -0500

    [PoseEstAnsar] Computing K

--- src/pose_est_Ansar.cpp
+++ src/pose_est_Ansar.cpp
@@ -11,9 +11,7 @@ namespace ky
 		const PointCloudUV& _uv, Matrix4f& _transformation)
 	{
 		_computeM(_model, _uv);
-
 		_computeK(_uv.size());
-
 
 		return 0;
 	}
@@ -36,9 +34,6 @@ namespace ky
 				auto dx{ _model[i].x - _model[j].x };
 				auto dy{ _model[i].y - _model[j].y };
 				auto dz{ _model[i].z - _model[j].z };
-				dx /= 100.f;
-				dy /= 100.f;
-				dz /= 100.f;
 				m_M(k, n * (n + 1) / 2) = -(dx*dx + dy*dy + dz*dz);
 				++k;
 			}
@@ -52,21 +47,23 @@ namespace ky
 		MatrixXf Q{ qr.matrixQ() };
 		size_t N{ n + 1 };
 		MatrixXf V{ Q.block(0, Q.cols() - N, Q.rows(), N) };  // V: Ker(M)
-		m_K = MatrixXf(N * (N + 1) / 2, N);  
+		m_K = MatrixXf(n * n * (n - 1) / 2, (n + 1) * (n + 2) / 2);
 
-		std::vector<std::tuple<size_t, size_t, size_t> > ijk;
-		for (size_t i{ 0 }; i < n; ++i)
-			for (size_t j{ 0 }; j < n - 1; ++j)
-				for (size_t k{ j + 1 }; k < n; ++k)
-					if (i != j && i != k)
-						ijk.emplace_back(i, j, k);
-
+		std::vector<std::vector<size_t> > iijk, ijik;
 		for (size_t i{ 0 }; i < n; ++i)
 			for (size_t j{ 0 }; j < n; ++j)
 				if (i != j)
-					ijk.emplace_back(i, j, j);
+					for (size_t k{ j }; k < n; ++k)
+						if (i != k)
+						{
+							iijk.emplace_back(std::vector<size_t>{i, i, j, k});
+							std::vector<size_t> idx{ i, j, i, k };
+							if (i > j) { std::swap(idx[0], idx[1]); }
+							if (i > k) { std::swap(idx[2], idx[3]); }
+							ijik.push_back(idx);
+						}
 
-		size_t row{ 0 }, col{ 0 };
+		size_t row{ 0 };
 		std::map<std::tuple<size_t, size_t>, size_t> x;
 		for (size_t i{ 0 }; i < n - 1; ++i)
 			for (size_t j{ i + 1 }; j < n; ++j)
@@ -75,28 +72,21 @@ namespace ky
 		for (size_t i{ 0 }; i < n; ++i)
 			x.insert(std::pair<std::tuple<size_t, size_t>, size_t>(std::make_tuple(i, i), row++));
 
-		for (const auto& idx : ijk)
+		row = 0;
+		for (; row < m_K.rows(); ++row)
 		{
-			size_t i{ std::get<0>(idx) }, j{ std::get<1>(idx) }, k{ std::get<2>(idx) };
-			std::tuple<size_t, size_t> ij{ std::make_tuple(i, i) };
-			std::tuple<size_t, size_t> kl{ std::make_tuple(j, k) };
-			std::tuple<size_t, size_t> _ij{ i > j ? std::make_tuple(j, i) : std::make_tuple(i, j) };
-			std::tuple<size_t, size_t> _kl{ i > k ? std::make_tuple(k, i) : std::make_tuple(i, k) };
+			std::tuple<size_t, size_t> ij{ std::make_tuple(iijk[row][0], iijk[row][1]) };
+			std::tuple<size_t, size_t> kl{ std::make_tuple(iijk[row][2], iijk[row][3]) };
+			std::tuple<size_t, size_t> pq{ std::make_tuple(ijik[row][0], ijik[row][1]) };
+			std::tuple<size_t, size_t> rs{ std::make_tuple(ijik[row][2], ijik[row][3]) };
 
-			row = 0;
-			for (size_t a{ 0 }; a < N - 1; ++a)
-				for (size_t b{ a + 1 }; b < N; ++b)
-					m_K(row++, col) = 2 * (V(x.at(ij), a) * V(x.at(kl), b) - V(x.at(_ij), a) * V(x.at(_kl), b));
-
-			for (size_t a{ 0 }; a < N; ++a)
-				m_K(row++, col) = V(x.at(ij), a) * V(x.at(kl), a) - V(x.at(_ij), a) * V(x.at(_kl), a);
-
-			if (++col == N)
-				break;
+			for (size_t c{ 0 }, a{ 0 }; a < N; ++a)
+				for (size_t b{ a }; b < N; ++b)
+					if (a == b)
+						m_K(row, c++) = (V(x.at(ij), a) * V(x.at(kl), b) - V(x.at(pq), a) * V(x.at(rs), b));
+					else
+						m_K(row, c++) = (V(x.at(ij), a) * V(x.at(kl), b) - V(x.at(pq), a) * V(x.at(rs), b)) +	
+						(V(x.at(kl), a) * V(x.at(ij), b) - V(x.at(rs), a) * V(x.at(pq), b));
 		}
-
-		// TO-DO: revise contructing K.
-		JacobiSVD<MatrixXf> svd(m_K, ComputeThinU | ComputeThinV);
-		VectorXf lambda = svd.matrixV().col(N - 1);
 	}
 }
